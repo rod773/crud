7bfc7ef40b092838524b5137923dffb540d509aa
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-17 11:53:59 +0200

    producto repository

--- src/main/java/com/tutorial/crud/repository/ProductoRepository.java
+++ src/main/java/com/tutorial/crud/repository/ProductoRepository.java
@@ -2,7 +2,10 @@ package com.tutorial.crud.repository;
 
 import com.tutorial.crud.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto,Integer> {
 
 

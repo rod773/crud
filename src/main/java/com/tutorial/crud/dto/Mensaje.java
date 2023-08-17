cbb470c168b8e34fee73d04f0eb333ef82f9bb98
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-17 12:55:17 +0200

    producto service

--- src/main/java/com/tutorial/crud/dto/Mensaje.java
+++ src/main/java/com/tutorial/crud/dto/Mensaje.java
@@ -1,4 +1,9 @@
 package com.tutorial.crud.dto;
 
 public class Mensaje {
+    private String mensaje;
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
 }
--- src/main/java/com/tutorial/crud/repository/ProductoRepository.java
+++ src/main/java/com/tutorial/crud/repository/ProductoRepository.java
@@ -4,9 +4,12 @@ import com.tutorial.crud.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto,Integer> {
 
-
+    Optional<Producto> findByNombre(String nombre);
+    boolean existByNombre(String nombre);
 }
